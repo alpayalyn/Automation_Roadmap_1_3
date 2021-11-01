2e07177225853c18aecfcc9e416c9237c08ff0ae
alpayalyn <alpay.alin@useinsider.com>
2021-11-02 01:02:41 +0300

    Kodda sadelestirme yapıldı.

--- main.py
+++ main.py
@@ -1,83 +1,50 @@
-vize1= int(input("Lütfen vize1 notunuzu giriniz:"))
-vize2= int(input("Lütfen vize2 notunuzu giriniz:"))
-final= int(input("Lütfen final notunuzu giriniz:"))
+def validation(point):
+    while True:
+        if point < 0 or point > 100:
+            point = int(input("Hata! Lütfen notunuzu 0-100 arası tekrar giriniz:"))
+        else:
+            break            
+    return point
 
-def vize1notu(vize1):
+vize1 = validation(int(input("Lütfen vize1 notunuzu giriniz:")))
+vize2 = validation(int(input("Lütfen vize2 notunuzu giriniz:")))
+final = validation(int(input("Lütfen final notunuzu giriniz:")))
 
-    if ((vize1 >= 0) and (vize1 <= 100)):
-        vize1yuzde = 30
-        vize1etki = vize1 * vize1yuzde/100
+def notlama(vize1, vize2, final):
+    vize1yuzde = 30
+    vize2yuzde = 30
+    finalyuzde = 40
+    vize1etki = vize1 * vize1yuzde /100
+    vize2etki = vize2 * vize2yuzde /100
+    finaletki = final * finalyuzde /100
 
-    else:
-        print("Vize1 notunuzu geçersiz girdiniz. Bu yüzden bu kısım 0 puan olarak değerlendirilecektir.")
-        vize1etki = 0
-    print("Vize 1 etki notu:", vize1etki)
+    return vize1etki + vize2etki + finaletki
 
-    return vize1etki
+totalPoint = notlama(vize1, vize2, final)
 
-def vize2notu(vize2):
+if(totalPoint >= 90):
+    print("Aldığınız Harf notu AA'dır ve Genel puanınız:", totalPoint)
 
-    if ((vize2 >= 0) and (vize2 <= 100)):
-        vize2yuzde = 30
-        vize2etki = vize2 * vize2yuzde / 100
+elif(totalPoint < 90 and totalPoint >= 85):
+    print("Aldığınız Harf notu BA'dır ve Genel puanınız:", totalPoint)
 
-    else:
-        print("Vize2 notunuzu geçersiz girdiniz. Bu yüzden bu kısım 0 puan olarak değerlendirilecektir.")
-        vize2etki = 0
-    print("Vize 2 etki notu:", vize2etki)
+elif(totalPoint < 85 and totalPoint >= 80):
+    print("Aldığınız Harf notu BB'dır ve Genel puanınız:", totalPoint)
 
-    return vize2etki
+elif(totalPoint < 80 and totalPoint >= 75):
+    print("Aldığınız Harf notu CB'dır ve Genel puanınız:", totalPoint)
 
-def finalnotu(final):
+elif(totalPoint < 75 and totalPoint >= 70):
+    print("Aldığınız Harf notu CC'dır ve Genel puanınız:", totalPoint)
 
-    if ((final >= 0) and (final <= 100)):
-        finalyuzde = 40
-        finaletki = final * finalyuzde / 100
+elif(totalPoint < 70 and totalPoint >= 65):
+    print("Aldığınız Harf notu DC'dır ve Genel puanınız:", totalPoint)
 
-    else:
-        print("Final notunuzu geçersiz girdiniz. Bu yüzden bu kısım 0 puan olarak değerlendirilecektir.")
-        finaletki = 0
-    print("final etki notu:", finaletki)
+elif(totalPoint < 65 and totalPoint >= 60):
+    print("Aldığınız Harf notu DD'dır ve Genel puanınız:", totalPoint)
 
-    return finaletki
+elif(totalPoint < 60 and totalPoint >= 55):
+    print("Aldığınız Harf notu FD'dır ve Genel puanınız:", totalPoint)
 
-v1=vize1notu(vize1)
-v2=vize2notu(vize2)
-fi=finalnotu(final)
-
-Genel_puan = v1 + v2 + fi
-
-if(Genel_puan >= 90):
-    print("Aldığınız Harf notu AA'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan < 90 and Genel_puan >= 85):
-    print("Aldığınız Harf notu BA'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan < 85 and Genel_puan >= 80):
-    print("Aldığınız Harf notu BB'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan < 80 and Genel_puan >= 75):
-    print("Aldığınız Harf notu CB'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan < 75 and Genel_puan >= 70):
-    print("Aldığınız Harf notu CC'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan < 70 and Genel_puan >= 65):
-    print("Aldığınız Harf notu DC'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan < 65 and Genel_puan >= 60):
-    print("Aldığınız Harf notu DD'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan < 60 and Genel_puan >= 55):
-    print("Aldığınız Harf notu FD'dır ve Genel puanınız:", Genel_puan)
-
-elif(Genel_puan <55):
-    print("Aldığınız Harf notu FF'dır ve Genel puanınız:", Genel_puan)
-#tek fonksiyon ile not hesaplayalım.
-#Notları isterken while kullanalım.
-#variable isimleri düzeltelim.
-#refactor
-
-
-
-
+elif(totalPoint <55):
+    print("Aldığınız Harf notu FF'dır ve Genel puanınız:", totalPoint)
